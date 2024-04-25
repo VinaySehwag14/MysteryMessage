233fd0054bc20edfe0c89c7d6f71508bc6299c79
VinaySehwag14 <vinaysehwag14@gmail.com>
2024-04-25 06:51:46 +0530

    connected db and added api's for sign-up and verifying code

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -14,7 +14,6 @@ async function dbConnect(): Promise<void> {
 
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI || "");
-    console.log(db, "DB😍");
     connection.isConnected = db.connections[0].readyState;
 
     console.log("DB Connected Successfully!!");
